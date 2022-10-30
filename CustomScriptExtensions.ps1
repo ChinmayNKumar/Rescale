e749ece084899ca1a124378d60327e6a1beafd99
ChinmayNK <cnk.1991@gmail.com>
2022-10-30 19:21:31 -0400

    Add files via upload

---
+++ CustomScriptExtensions.ps1
@@ -0,0 +1,68 @@
+﻿#Script that demonstrates how to deploy apps to an Azure VM using Custom Script Extensions
+
+#Install-Module AzureRM - Install the AzureRM module if not installed
+Import-Module AzureRM #Import the AzureRM module
+
+#Use a service principal (non-interactive Azure account) with limited permissions for more security
+
+$pscredential = Get-Credential #Get the service principal's credentials as an object
+Connect-AzureRmAccount -ServicePrincipal -Credential $pscredential -TenantId $tenantid
+
+#Resource Group setup
+New-AzureRmResourceGroup -Name VMLab -Location EastUS#Storage Account setup$storageAccount = New-AzureRmStorageAccount -ResourceGroupName VMLab `
+-Name "CKTest" `
+-Location EastUS `-SkuName Standard_LRS `-Kind Storage
+
+#Set Storage Account Context
+$ctx = $storageAccount.Context
+
+#Blob container setup for the installer script
+New-AzureStorageContainer -Name scripts -Context $ctx -Permission blob
+
+#File share setup for the installer (msi/exe)
+New-AzureStorageShare `
+ -Name fileshare `
+ -Context $ctx
+
+#Creating a directory in the file share for storing the installers
+New-AzureStorageDirectory `
+ -Context $ctx `
+ -ShareName "fileshare" `
+ -Path "AntivirusSoftware" #Uploading the installer exe to the file share above. We're using Sophos Free Antivirus as an example Set-AzureStorageFileContent `
+ -Context $ctx `
+ -ShareName "fileshare" `
+ -Source "pathtoinstaller" `
+ -Path "AntivirusSoftware/SophosInstall.exe"#The InstallSophos.ps1 script will contain the following:#$password = Get-AzureRmStorageAccountKey -ResourceGroupName "VMLab" -AccountName "CKTest"#cmdkey /add:CKTest.file.core.windows.net /user:AZURE\CKTest /pass:$password 
+#\\CKTest.file.core.windows.net\fileshare\AntivirusSoftware\SophosInstall.exe /SP- /SILENT /NOCANCEL#Uploading the installer script to the blob containerSet-AzureStorageBlobContent `
+-File "C:\users\chinm\Downloads\InstallSophos.ps1" `
+-Container scripts `
+-Blob "InstallSophos.ps1" `
+-Context $ctx
+
+#Create a VM. Alternatively, we can use an image if available
+
+New-AzureRmVm `
+ -ResourceGroupName "VMLab" `
+ -Name "testVM" `
+ -Location "EastUS" `
+ -VirtualNetworkName "Vnet" `
+ -SubnetName "Subnet" `
+ -SecurityGroupName "SG" `
+ -PublicIpAddressName "PublicIP" `
+ -Credential $pscredential
+
+#Deploy Sophos to the VM
+
+$storageKey = Get-AzureRmStorageAccountKey -ResourceGroupName "VMLab" -AccountName "CKTest"
+
+ Set-AzureRmVmCustomScriptExtension `
+-ResourceGroupName VMLab `
+-Location EastUS `
+-VMName testVM `
+-Name InstallSophos `
+-TypeHandlerVersion "1.9" `
+-StorageAccountName CKTest `
+-StorageAccountKey $storageKey `
+-FileName InstallSophos.ps1 `
+-ContainerName scripts `
+-Run InstallSophos.ps1#Disconnect AzureRm sessionDisconnect-AzureRmAccount
---
+++ IntelMPI.ps1
@@ -0,0 +1,30 @@
+﻿#Script to show how Intel MPI HPC ndoes work with AD auth.
+#This script shows how to use domain-based authorization method with the delegation ability
+
+#Import the AD module
+Import-Module ActiveDirectory
+
+#Login to the DC as a Domain Admin (Start a remote session on the DC)
+$s = New-PSSession -ComputerName DC
+Enter-PSSession -Session $s
+
+#Per documentation, Enable the delegation for cluster nodes and Trust this computer for delegation to any service (Kerberos only)
+Get-ADComputer -Identity Node1,Node2 | Set-ADAccountControl ‑TrustedForDelegation $true
+#Repeat the steps for other nodes
+
+#Per documentation, Disable the account is sensitive and cannot be delegated option.
+Set-ADAccountControl -Identity MPIService -AccountNotDelegated $False
+
+#Register service principal name (SPN) for cluster nodes. Adds the specified SPN for the computer, after verifying that no duplicates exist.
+setspn.exe -S impi_hydra/node1:8679/impi_hydra node1
+
+#Login to each ndoe and execute
+#hydra_service -register_spn
+
+#Set user authorization method to Delegate using env. variable
+I_MPI_AUTH_METHOD=delegate
+
+#Checking accessiblity of the hosts by running a simple command (hostname)
+mpiexec -ppn 1 -n 2 -hosts node1,node2 hostname
+
+Exit-PSSession
